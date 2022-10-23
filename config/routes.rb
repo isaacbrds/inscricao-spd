e5429a8c38d4ae9d996f4714e3383e2bc62c9998
isaacbrds <isaac.brigido@gmail.com>
2022-10-23 17:30:40 -0300

    feat: create structure of api

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :subscripts
+  resources :subscripts, only: %i(index create new)
+  namespace :api do
+    namespace :v1 do
+      resources :subscripts, only: :index
+    end
+  end
   # Defines the root path route ("/")
   root 'home#index'
 end
